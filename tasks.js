644f73b17cdb2c54fb01a3c6fafe8407ecedf962
hussein010 <abouhachemhussein@gmail.com>
2021-06-10 14:33:08 +0300

    help command

--- tasks.js
+++ tasks.js
@@ -40,6 +40,9 @@ function onDataReceived(text) {
   else if(text === 'hello\n'){
     hello();
   }
+  else if(text === 'help\n'){
+    help();
+  }
   else{
     unknownCommand(text);
   }
@@ -76,6 +79,10 @@ function hello(){
 function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
+}
+// Listing all the possible commands 
+function help(){
+  console.log('hello \nexit \nquit')
 }
 
 // The following line starts the application
